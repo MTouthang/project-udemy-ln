53b1211f9214de9553b318666cd581d9fef242bf
MTouthang <53576865+MTouthang@users.noreply.github.com>
2023-08-14 23:57:52 +0530

    updated registered function

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,4 +1,6 @@
 import asyncHandler from "@/middlewares/asynHandler.middleware";
+import User from "@/models/user.model";
+import AppError from "@/utils/AppError.util";
 import { Request, Response, NextFunction } from "express";
 
 // cookie Properties value
@@ -15,9 +17,44 @@ const cookieOptions = {
  * @ACCESS Public
  */
 
-// export const registerUser = asyncHandler (async (req: Request, res: Response, next: NextFunction) => {
+export const registerUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    // const {firstName, lastName, email, password, role, avatar: {
+    //     public_id: email,
+    //     secure_url: "https://res.cloudinary.com/ddvlwqjuy/image/upload/v1692032956/project-udemy/profile_lzadwv.png"
+    // }} = req.body
 
-//     const {firstName, lastName, email, password, role, avatar: {
-//         secure_url: "put default url"
-//     }} = req.body
-// })
+    const { firstName, lastName, email, password } = req.body();
+
+    const isUserExist = await User.findOne({ email }).lean();
+
+    if (isUserExist) {
+      return next(
+        new AppError("User with the given email already registered!", 409),
+      );
+    }
+
+    const user = await User.create({
+      email,
+      firstName,
+      lastName,
+      password,
+      avatar: {
+        public_id: email,
+        secure_url:
+          "https://res.cloudinary.com/ddvlwqjuy/image/upload/v1692032956/project-udemy/profile_lzadwv.png",
+      },
+    });
+
+    if (!user) {
+      return next(
+        new AppError("User Registration failed, please try again", 400),
+      );
+    }
+
+    //TODO: send mail after registered
+
+    // const accessToken = await user.generateAccessToken()
+    // const refreshToken = await user.generateRefreshToken()
+  },
+);
